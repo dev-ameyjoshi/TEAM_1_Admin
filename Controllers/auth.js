c58ad337b2fa19b18dbbb86a7fb16846f2320500
SaharshSamir <saharshsamir10@gmail.com>
2021-07-21 10:43:34 +0530

    Added SignUp page

---
+++ Controllers/auth.js
@@ -0,0 +1,6 @@
+const signUp = (req, res) => {
+    console.log("routes' working");
+    res.status(200).send("success");
+}
+
+exports.signUp = signUp;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const authRoutes = require("./Routes/authRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,7 +18,13 @@ app.use(express.static('public'));
 
 app.get("/", (req, res) => {
     res.render("main", {layout: "index"});
-})
+});
+
+app.get("/signUp", (req, res) => {
+    res.render("signUp", {layout: "index"});
+});
+app.use("/auth", authRoutes);
+
 
 
 app.listen(PORT, () => {
